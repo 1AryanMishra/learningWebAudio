7655088e36ba70453a955c53e6d3466b65909415
Aryan Mishra <mishra321123@gmail.com>
2021-09-08 16:17:49 +0530

    firstCommit

---
+++ script.js
@@ -0,0 +1,75 @@
+const visualizer = document.getElementById('visualizer')
+var freq = document.getElementById("freq");
+
+
+var freqHistory = [];
+
+const context = new AudioContext()
+const analyserNode = new AnalyserNode(context, { fftSize: 256 })
+
+
+setupContext()
+resize()
+drawVisualizer()
+
+
+async function setupContext() {
+  const guitar = await getGuitar()
+  if (context.state === 'suspended') {
+    await context.resume()
+  }
+  const source = context.createMediaStreamSource(guitar)
+  source
+    .connect(analyserNode)
+}
+
+function getGuitar() {
+  return navigator.mediaDevices.getUserMedia({
+    audio: {
+      echoCancellation: false,
+      autoGainControl: false,
+      noiseSuppression: false,
+      latency: 0
+    }
+  })
+}
+
+function drawVisualizer() {
+  requestAnimationFrame(drawVisualizer)
+
+  const bufferLength = analyserNode.frequencyBinCount
+  const dataArray = new Uint8Array(bufferLength)
+  analyserNode.getByteFrequencyData(dataArray)
+  const width = visualizer.width
+  const height = visualizer.height
+  const barWidth = width / bufferLength
+
+  const canvasContext = visualizer.getContext('2d')
+  canvasContext.clearRect(0, 0, width, height)
+
+  var maxi = dataArray[0];
+  dataArray.forEach((item, index) => {
+    if(item > maxi){
+      maxi = item
+    }
+  })
+
+  freqHistory.push(maxi);
+  freq.textContent = `${maxi}`
+
+  for(var i = 0; i < freqHistory.length; i++){    
+    canvasContext.fillStyle = `#fff`;
+    const y = (freqHistory[i]/255)*(height);
+    canvasContext.fillRect(i, y, 2, 2);
+  }
+  canvasContext.fillRect(freqHistory.length, height, height, 1)
+
+  if(freqHistory.length > 256){
+    freqHistory.splice(0, 1);
+  }
+}
+
+function resize() {
+  visualizer.width = visualizer.clientWidth * window.devicePixelRatio
+  visualizer.height = visualizer.clientHeight * window.devicePixelRatio
+}
