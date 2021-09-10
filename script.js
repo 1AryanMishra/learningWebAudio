fb714da4cab171186fc11962c79a8f23a71a9c05
Aryan Mishra <mishra321123@gmail.com>
2021-09-10 17:38:16 +0530

    i think this is it

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const visualizer = document.getElementById('visualizer')
 var freq = document.getElementById("freq");
+var arr = document.getElementById("arr");
+
 
 
 var freqHistory = [];
 
 const context = new AudioContext()
-const analyserNode = new AnalyserNode(context, { fftSize: 256 })
-
+const analyserNode = new AnalyserNode(context)
+const sr = context.sampleRate;
+const ffts = analyserNode.fftSize;
 
 setupContext()
 resize()
-drawVisualizer()
-
+drawVisualizer();
 
 async function setupContext() {
   const guitar = await getGuitar()
@@ -20,46 +22,48 @@ async function setupContext() {
   }
   const source = context.createMediaStreamSource(guitar)
   source
-    .connect(analyserNode)
+  .connect(analyserNode)
 }
 
 function getGuitar() {
   return navigator.mediaDevices.getUserMedia({
     audio: {
-      echoCancellation: false,
-      autoGainControl: false,
-      noiseSuppression: false,
-      latency: 0
+      latency: 0,
+      echoCancellation : false,
+      autoGainControl : false,
+      noiseSuppression : false
     }
   })
 }
 
-function drawVisualizer() {
-  requestAnimationFrame(drawVisualizer)
 
+function drawVisualizer() {
+
+  requestAnimationFrame(drawVisualizer);
+  
   const bufferLength = analyserNode.frequencyBinCount
-  const dataArray = new Uint8Array(bufferLength)
-  analyserNode.getByteFrequencyData(dataArray)
+  const dataArray = new Float32Array(bufferLength)
+  analyserNode.getFloatFrequencyData(dataArray)
   const width = visualizer.width
   const height = visualizer.height
   const barWidth = width / bufferLength
 
   const canvasContext = visualizer.getContext('2d')
   canvasContext.clearRect(0, 0, width, height)
-
-  var maxi = dataArray[0];
+  
+  var maxi = 0;
   dataArray.forEach((item, index) => {
-    if(item > maxi){
-      maxi = item
+    if(item > dataArray[maxi]){
+      maxi = index;
     }
   })
-
-  freqHistory.push(maxi);
-  freq.textContent = `${maxi}`
-
+  
+  freqHistory.push(maxi*(sr/ffts));
+  freq.textContent = `${maxi*(sr/ffts)}`
+  
   for(var i = 0; i < freqHistory.length; i++){    
     canvasContext.fillStyle = `#fff`;
-    const y = height - (freqHistory[i]/256)*(height);
+    const y = height - (freqHistory[i]/1024)*(height);
     canvasContext.fillRect(i, y, 2, 2);
   }
   canvasContext.fillRect(freqHistory.length, height, height, 1)
@@ -73,3 +77,4 @@ function resize() {
   visualizer.width = visualizer.clientWidth * window.devicePixelRatio
   visualizer.height = visualizer.clientHeight * window.devicePixelRatio
 }
+
